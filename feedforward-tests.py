f0c5619f058ce730bc55ec2e5b93eb5befecb9cd
Adam Selker <adamselker@gmail.com>
2017-12-04 23:10:19 -0500

    Working through part 1

--- feedforward-tests.py
+++ feedforward-tests.py
@@ -12,11 +12,11 @@ import feedforward as ff
 def tanh(x):
   return ( exp(x) - exp(-x) ) / ( exp(x) + exp(-x) )
 
-def tanh_test():
-  # Because we are working with floating-point numbers, different, valid ways of computing the same thing can give slightly different
-  #   answers.  So, we need a definition of equality that has some slop.  We use this epsilon as the smallest allowable difference.
-  epsilon = 0.0001
+# Because we are working with floating-point numbers, different, valid ways of computing the same thing can give slightly different
+#   answers.  So, we need a definition of equality that has some slop.  We use this epsilon as the smallest allowable difference.
+epsilon = 0.0001
 
+def tanh_test():
   for i in [-1, 0, 1, 2]: # Try four cases
     # If the function hasn't been implemented yet, or if the implementations differ, return false.
     if (ff.tanh(i) is None) or (abs(tanh(i) - ff.tanh(i)) > epsilon):
@@ -31,3 +31,36 @@ else:
   print("tanh fail")
 
 
+fiveNeuronReferenceOutput = tanh( tanh(0.5*1 + 1*0.5) * 2 + tanh(0.5*0.5 + 1*2) * 1 )
+
+def neuron_test():
+  input_1 = ff.Neuron(None, None)
+  input_2 = ff.Neuron(None, None)
+  hidden_1 = ff.Neuron([input_1, input_2], [1, 0.5])
+  hidden_2 = ff.Neuron([input_1, input_2], [0.5, 2])
+  output_1 = ff.Neuron([hidden_1, hidden_2], [2, 1])
+
+  input_1.activation = 0.5
+  input_2.activation = 1
+  hidden_1.evaluate()
+  hidden_2.evaluate()
+  output_1.evaluate()
+
+  if abs(fiveNeuronReferenceOutput - output_1.activation) > epsilon:
+    return False
+  else:
+    return True
+
+if neuron_test():
+  print("Neuron class pass")
+else:
+  print("Neuron class fail")
+
+
+def simpleNetwork_test():
+  return (ff.simpleNetwork() is not None) and (abs(fiveNeuronReferenceOutput - ff.simpleNetwork()) < epsilon)
+
+if simpleNetwork_test():
+  print("Simple network pass")
+else:
+  print("Simple network fail")
--- feedforward.py
+++ feedforward.py
@@ -1,12 +1,12 @@
 """
-These functions -- some pre-written, some fill-in-the-blanks -- should be almost all you need to write a feedforward neural network.
+These functions -- some pre-written, some fill-in-the-blanks -- should be almost all you need to write a simple neural network.
 It won't learn, though.  That's the next section.
 
 A few useful tips:
   * print() everything that you're not sure about.
   * Make sure each function passes its tests before using it.
-  * Use "python -i script-name.py" to run the script, and then put you at a Python command line after it's done.  This way, you can look at all of the variables you set and call the functions you wrote.
-  * If you really can't figure a function out, and you want a reference implementation, look in feedforward-tests.py.
+  * Use "python -i script-name.py" to run the script, and then put you at a Python command line after it's done.  This 
+      way, you can look at all of the variables you set and call the functions you wrote.
 
 """
 
@@ -14,4 +14,50 @@ from math import exp, log # These are e^x and ln(x), respectively
 
 def tanh(x):
   pass
+
+
+class Neuron:
+  
+  # Creating a neuron takes two pieces of information: which other neurons this one gets its input from, and the weight
+  #   for each of those connections.  So, we pass them into __init__.  If the neuron is an input neuron, its inputs and
+  #   weights should both be set to None.
+  def __init__(self, inputs, weights):
+    self.inputs = inputs #A list of neurons that this one takes its inputs from
+    self.weights = weights #A list of weights. weight[i] corresponds to input[i].
+
+    self.activation = 0 #This should get reset in evaluate()
+
+  def evaluate(self):
+    if self.inputs is None:
+      return #If this is an input neuron, don't update it.  It doesn't have anywhere to pull its input from!
+    else:
+      pass #You do the rest!
+
+
+# This is the five-neuron network from Problems 2 and 4.  It should return the output neuron's activation.
+def simpleNetwork():
+  pass
+
+
+# Helper functions for working with letters
+
+letters = "abcdefghijklmnopqrstuvwxyz _"
+
+def letterToList(x):
+  index = letters.index(x)
+  return np.array([lowState]*index + [highState] + [lowState]*(len(letters)-index-1))
+
+def listToLetters(xs):
+  tuples = [ i for i in list(zip(xs,letters)) ]
+  tuples = sorted(tuples, key=lambda x: (-x[0],x[1])) #Sort reverse by first value, forward by second
+  return [x[1] for x in tuples] #Return just the letters
+
+def printLetterWeights(xs, removeZeros = False):
+  tuples = [i for i in list(zip(xs,letters)) if (i[0] > 0.01 or not removeZeros)] #Remove the ~zeroes
+  tuples = sorted(tuples, key=lambda x: (-x[0],x[1])) #Sort reverse by first value, forward by second
+  for i in tuples:
+    if len(str(i[0])) > 5:
+      print(str(i[0])[0:6] + ":\t" + str(i[1])) #Limit digits
+    else:
+      print(str(i[0]) + ":\t" + str(i[1])) #All the digits
 
